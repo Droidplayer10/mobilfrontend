e8f9f131a6d9d1d29dd7acdacdcfaf1d2eb89c96
Droidplayer10 <droidplayer10@gmail.com>
2023-04-01 23:47:55 +0200

    MyScreen torlese

--- Screens/BejelentkezettProfileScreen.js
+++ Screens/BejelentkezettProfileScreen.js
@@ -14,17 +14,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const BejelentkezettProfileScreen = ({navigation}) => {
  
   const [felhasznaloId, setFelhasznaloId] = useState(null);
-  const [isMenuOpen, setIsMenuOpen] = useState(true);
-  const [menuAnim] = useState(new Animated.Value(1));
+  
 
-  const toggleMenu = () => {
-    Animated.timing(menuAnim, {
-      toValue: isMenuOpen ? 0 : 1,
-      duration: 500,
-      useNativeDriver: true,
-      
-    }).start(() => setIsMenuOpen(!isMenuOpen));
-  };
+ 
   AsyncStorage.getItem("felhasznalo_id")
   .then((value) => {
     setFelhasznaloId(value);
@@ -83,28 +75,6 @@ resizeMode={"cover"}
         <Text style={{ fontSize: 24, fontWeight: 'bold' }}>IT Travel&Relax</Text>
       </View>
       
-      {isMenuOpen && (
-        <Animated.View style={{ paddingVertical: 20, paddingHorizontal: 20, opacity: menuAnim }}>
-          <TouchableOpacity style={{ paddingVertical: 10 }}>
-            <Text style={{ fontSize: 18 }}>Kedvencek</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{ paddingVertical: 10 }}>
-            <Text style={{ fontSize: 18 }}>Előzmények</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{ paddingVertical: 10 }}>
-            <Text style={{ fontSize: 18 }}>Ajánlataink</Text>
-          </TouchableOpacity>
-        </Animated.View>
-        
-      )}
-      <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-        <TouchableOpacity
-          onPress={toggleMenu}
-          style={{ backgroundColor: '#007aff', borderRadius: 20, paddingVertical: 10, paddingHorizontal: 20 }}
-        >
-          <Text style={{ color: 'white' }}>{isMenuOpen ? 'Bezárás' : 'Menü'}</Text>
-        </TouchableOpacity>
-      </View>
    
 
         <View style={styles.alternativeLayoutButtonContainer} >
